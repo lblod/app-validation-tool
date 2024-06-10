fad0b426481e2734c031bf87997f916fa0383a43
root <LBONTEN>
2024-06-10 07:49:02 +0000

    complete setup pt II

---
+++ config/dispatcher/dispatcher.ex
@@ -0,0 +1,34 @@
+defmodule Dispatcher do
+  use Matcher
+  define_accept_types [
+    html: [ "text/html", "application/xhtml+html" ],
+    json: [ "application/json", "application/vnd.api+json", "application/sparql-results+json" ]
+  ]
+
+  @any %{}
+  @json %{ accept: %{ json: true } }
+  @html %{ accept: %{ html: true } }
+
+
+  ###############
+  # FRONTEND
+  ###############
+  match "/assets/*path", @any do
+    Proxy.forward conn, path, "http://frontend/assets/"
+  end
+
+  match "/@appuniversum/*path", @any do
+    Proxy.forward conn, path, "http://frontend/@appuniversum/"
+  end
+
+  match "/*_path", @html do
+    Proxy.forward conn, [], "http://frontend/index.html"
+  end
+
+  #################
+  # NOT FOUND
+  #################
+  match "/*_", %{ last_call: true } do
+    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  end
+end
